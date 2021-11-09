bcbc42f438b82ed56886fcf8ef38013a51364da3
Vigneshramm2021 <vignesh.ram@yahoo.com>
2021-11-09 18:46:00 +0530

    password issue resolved

--- src/Gitpush.java
+++ src/Gitpush.java
@@ -1,13 +1,13 @@
 
 public class Gitpush {
 
-	public void username() {
-		System.out.println("Username");
+	public void password() {
+		System.out.println("password");
 	}
 
 	public static void main(String[] args) {
 		Gitpush ref = new Gitpush();
-		ref.username();
+		ref.password();
 	}
 
 }
