5118cebdd37ef4bdfe7e8fab9dfc1b8e1cd42aaa
Vigneshramm2021 <vignesh.ram@yahoo.com>
2021-11-09 18:35:18 +0530

    Folder Structure

---
+++ src/Gitpush.java
@@ -0,0 +1,13 @@
+
+public class Gitpush {
+
+	public void username() {
+		System.out.println("Username");
+	}
+
+	public static void main(String[] args) {
+		Gitpush ref = new Gitpush();
+		ref.username();
+	}
+
+}
